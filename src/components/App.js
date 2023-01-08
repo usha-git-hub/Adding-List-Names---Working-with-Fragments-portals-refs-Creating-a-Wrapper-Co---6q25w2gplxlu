7c991f94d9243c1be753db31d8881800be3ac2c0
usha-git-hub <113238878+usha-git-hub@users.noreply.github.com>
2023-01-08 08:10:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,19 @@ import '../styles/App.css';
 import List from "./List";
 const App = () => {
   //code here
+   const [name, setname] = useState("");
+  const[list, setlist] = useState([]);
+
+  const handleChange =(e) => {
+    setname(e.target.value);
+  }
+  const handleOnClick =()=>{
+    // setlist([...name, list])
+    // list.push(name)
+    setname("");
+    setlist([...list, name])
+
+  }
   return (
     <div id="main">
        <input id="input" onChange={} />
